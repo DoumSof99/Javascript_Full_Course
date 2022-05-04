332a6b066d2e3bcadca4154dc373ffe40c2dc099
DoumSof99 <sofiadmn99@gmail.com>
2022-05-04 15:17:55 +0300

    Course

---
+++ 3-JS_Junior/6-Truthy_and_Falsy/app.js
@@ -0,0 +1,29 @@
+
+const bool1 = true;
+const bool2 = 2 > 1;
+
+if(bool1){
+    console.log('it works!');
+}
+if(bool2){
+    console.log('it works too!');
+}
+
+
+const text1 = 'sofia'
+
+if(text1){
+    console.log('it is true!');
+}
+else{
+    console.log('it is false!');
+}
+
+const text2 = '';
+
+if(text2){
+    console.log('it is true!');
+}
+else{
+    console.log('it is false!');
+}
