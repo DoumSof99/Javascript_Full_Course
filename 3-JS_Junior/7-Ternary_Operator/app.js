4d2fc47192c799c8bb54870cec84ac5d844c3900
DoumSof99 <sofiadmn99@gmail.com>
2022-05-04 15:24:46 +0300

    Course

---
+++ 3-JS_Junior/7-Ternary_Operator/app.js
@@ -0,0 +1,20 @@
+//unary operator - typeof
+let text = 'some text';
+console.log(typeof text);
+
+//binary operator - assignment
+let number = 3;
+let number2 = 2 + 5;
+
+//ternary operator 
+
+const value = 2 > 1;
+//const value = 2 < 1;
+
+value ? console.log('true') : console.log('flase');
+
+// if(value){
+//     console.log('true');
+// }else{
+//     console.log('false');
+// }
